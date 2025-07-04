473e864b77a2b71531a1870087226557ffb164c9
Lora-BabyPrabha <ranambabyprabha@gmail.com>
2025-07-04 20:36:44 +0530

    change

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .views import admin_dashboard 
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,4 +9,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact_view, name='contact'),
     path('jobs/<int:job_id>/', views.job_detail, name='job_detail'),
+    path('admin/login/', auth_views.LoginView.as_view(template_name='admin_login.html'), name='admin_login'),
+    path('admin/dashboard/', admin_dashboard, name='admin_dashboard'),
 ]
--- app/views.py
+++ app/views.py
@@ -66,3 +66,21 @@ def job_detail(request, job_id):
         form = JobApplicationForm()
 
     return render(request, 'job-details.html', {'job': job, 'form': form})
+
+# views.py
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render
+from .models import JobDetails, JobApplication
+
+def is_admin(user):
+    return user.is_staff or user.is_superuser
+
+@login_required
+@user_passes_test(is_admin)
+def admin_dashboard(request):
+    jobs = JobDetails.objects.all()
+    applications = JobApplication.objects.all()
+    return render(request, 'admin_dashboard.html', {
+        'jobs': jobs,
+        'applications': applications,
+    })
