62704ef7b4ddca9e8e65e70fced61bd7673f9c7c
lora-rakesh <garbhanirakeshkumar@gmail.com>
2025-07-04 23:03:30 +0530

    update

--- app/urls.py
+++ app/urls.py
@@ -31,6 +31,6 @@ urlpatterns = [
     # Manage Jobs
     path('dashboard/jobs/', views.manage_jobs, name='manage_jobs'),
     path('dashboard/jobs/add/', views.add_job, name='add_job'),
-    path('dashboard/jobs/edit/<int:pk>/', views.edit_job, name='edit_job'),
+    path('dashboard/jobs/edit/<int:job_id>/', views.edit_job, name='edit_job'),
     path('dashboard/jobs/delete/<int:pk>/', views.delete_job, name='delete_job'),
 ]
